d5fc9ff4f9e9a44d88d120d052e84ce75a4e3c11
Pranjali Bhardwaj <146981751+PranjaliBhardwaj@users.noreply.github.com>
2024-01-05 19:17:32 +0530

    Create dayfive.cpp

---
+++ January/dayfive.cpp
@@ -0,0 +1,20 @@
+/* Given an integer array nums, return the length of the longest strictly increasing subsequence. */
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>dp(n,0);
+
+        for(int i=0;i<n;i++){
+            int temp = 0;
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    temp=max(temp, dp[j]);
+                }
+            }
+            dp[i] = 1 + temp;
+        }
+
+        return *max_element(dp.begin(),dp.end());
+    }
+};
