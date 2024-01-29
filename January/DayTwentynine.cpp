eb5344ed664a48fe7d770daaf4e05ce634aeb506
Pranjali Bhardwaj <146981751+PranjaliBhardwaj@users.noreply.github.com>
2024-01-29 19:59:42 +0530

    Create DayTwentynine.cpp

---
+++ January/DayTwentynine.cpp
@@ -0,0 +1,51 @@
+/*Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+
+Implement the MyQueue class:
+
+void push(int x) Pushes element x to the back of the queue.
+int pop() Removes the element from the front of the queue and returns it.
+int peek() Returns the element at the front of the queue.
+boolean empty() Returns true if the queue is empty, false otherwise.*/
+
+
+class MyQueue {
+public:
+    stack<int>st1,st2; 
+    MyQueue() { 
+    }   
+    void push(int x) {
+        
+        while(!st1.empty()){
+            st2.push(st1.top());
+            st1.pop();
+        }
+        st1.push(x);
+        while(!st2.empty()){
+            st1.push(st2.top());
+            st2.pop();
+        }
+    }
+    
+    int pop() {
+        int x=st1.top();
+        st1.pop();
+        return x;
+    }
+    
+    int peek() {
+        return st1.top();
+    }
+    
+    bool empty() {
+        return st1.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
