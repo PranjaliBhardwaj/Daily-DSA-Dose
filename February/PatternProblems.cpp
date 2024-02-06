b1c94b294dff360c4595dcbc793f18bb0ec13b8d
Pranjali Bhardwaj <146981751+PranjaliBhardwaj@users.noreply.github.com>
2024-02-07 00:29:29 +0530

    Update PatternProblems.cpp

--- February/PatternProblems.cpp
+++ February/PatternProblems.cpp
@@ -85,6 +85,28 @@ cout<<endl;
 }
 }
 
+/*
+1         1
+1 2     2 1
+1 2 3 3 2 1 */
+void numberCrown(int n) {
+    int space = 2*(n-1);
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout<<j;
+        }
+        for(int j=1; j<=space; j++){
+            cout<<" ";
+        }
+        for(int j=i; j>=1; j--){
+            cout<<j;
+        }
+        cout<<endl;
+        space -= 2;
+    }
+}
+
+
 
 
 
