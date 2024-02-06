28f68d66b76e8d25c83127af0a6c26d73becf11b
Pranjali Bhardwaj <146981751+PranjaliBhardwaj@users.noreply.github.com>
2024-02-06 22:51:39 +0530

    Update PatternProblems.cpp

--- February/PatternProblems.cpp
+++ February/PatternProblems.cpp
@@ -23,6 +23,30 @@ cout<<endl;
 }
 
 
+
+/*   1
+     0 1
+     1 0 1  */
+void nBinaryTriangle(int n) {
+    int star=1;
+    for(int i=0; i<n; i++){
+        if(i%2 == 0){
+                star = 1;
+            } else {
+                star=0;
+            }
+        for(int j=0; j<=i; j++){
+            
+            cout<<star<<" ";
+        star= 1-star;
+        }
+        cout<<endl;
+    }
+}
+
+
+
+
 /*    1
       1 2
       1 2 3
