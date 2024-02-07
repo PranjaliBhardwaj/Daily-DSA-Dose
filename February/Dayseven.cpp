a3803c23d07b704c144df3c392af47e2ad9febee
Pranjali Bhardwaj <146981751+PranjaliBhardwaj@users.noreply.github.com>
2024-02-07 18:53:10 +0530

    Create Dayseven.cpp

---
+++ February/Dayseven.cpp
@@ -0,0 +1,33 @@
+/* Given a string s, sort it in decreasing order based on the frequency of the characters. The frequency of a character is the number of times it appears in the string.
+
+Return the sorted string. If there are multiple answers, return any of them.
+
+ */
+
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char, int> mp;
+        vector<pair<int, char>> v;
+        string ans = "";
+
+        // count character frequency
+        for(auto ch: s){
+            mp[ch]++;
+        }
+
+        // push from map to vector 
+        for(auto i: mp){
+            v.push_back({i.second, i.first});
+        }
+
+        // sort the vector in decreasing order
+        sort(v.begin(), v.end(), greater<pair<int, char>>());
+
+        // add to final answer string
+        for(auto i: v){
+            while(i.first--) ans += i.second;
+        }
+        return ans;
+    }
+};
