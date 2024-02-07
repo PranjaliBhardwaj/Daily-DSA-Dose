72db1c2dd8736403415ff273ec7c2425d5a08373
Pranjali Bhardwaj <146981751+PranjaliBhardwaj@users.noreply.github.com>
2024-02-07 23:08:57 +0530

    Update PatternProblem2.cpp

--- February/PatternProblem2.cpp
+++ February/PatternProblem2.cpp
@@ -69,6 +69,29 @@ void alphaRamp(int n) {
     }
 }
 
+/*     A
+     A B A
+   A B C B A  */
+
+void alphaHill(int n) {
+    for(int i=0; i<n; i++){
+        for(int j=0; j<=n-i-1;j++){
+            cout<<" ";
+        }
+        int breakpoint = (2*i+1)/2;
+        char ch='A';
+        for(int j=1; j<=2*i + 1; j++){
+            cout<<ch;
+            if(j<=breakpoint) ch++;
+            else ch--;
+        }
+        for(int j=0; j<=n-i-1;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+
 
 
 
