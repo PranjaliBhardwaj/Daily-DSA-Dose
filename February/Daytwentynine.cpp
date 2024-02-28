051bd66b40802b915058f6b7b7828fb18f926c11
Pranjali Bhardwaj <146981751+PranjaliBhardwaj@users.noreply.github.com>
2024-02-29 00:23:27 +0530

    Create Daytwentynine.cpp

---
+++ February/Daytwentynine.cpp
@@ -0,0 +1,26 @@
+/*Given the root of a binary tree, return the leftmost value in the last row of the tree.*/
+
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        int leftmost_value;
+
+        while (!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
+
+            leftmost_value = node->val;
+
+            if (node->right) {
+                q.push(node->right);
+            }
+            if (node->left) {
+                q.push(node->left);
+            }
+        }
+
+        return leftmost_value;
+    }
+};
