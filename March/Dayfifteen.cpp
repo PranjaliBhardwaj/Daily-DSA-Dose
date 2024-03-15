693cd9419eef60b4cd53bc44e4209168770d841c
Pranjali Bhardwaj <146981751+PranjaliBhardwaj@users.noreply.github.com>
2024-03-15 22:38:49 +0530

    Create Dayfifteen.cpp

---
+++ March/Dayfifteen.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> ans(n);
+        ans[0]=1;
+
+        //prefix
+        for(int i=1;i<n;i++){
+            ans[i]=ans[i-1] * nums[i-1];
+        }
+
+        // suffix
+        int suffix=1;
+        for(int i=n-1;i>=0;i--){
+            ans[i]=ans[i]*suffix;
+            suffix=suffix*nums[i];
+        }
+
+        return ans;
+    }
+};
