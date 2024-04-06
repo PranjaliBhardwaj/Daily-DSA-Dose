11d389a4eebd7c87a0f31bbb4ea285c8e0543fc2
Pranjali Bhardwaj <146981751+PranjaliBhardwaj@users.noreply.github.com>
2024-04-06 15:31:29 +0530

    Create Daysix.cpp

---
+++ April/Daysix.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        stack<int> stack;
+
+        for (int i = 0; i < s.length(); ++i)
+            if (s[i] == '(') {
+                stack.push(i);
+            } else if (s[i] == ')') {
+                if (stack.empty())
+                    s[i] = '*';
+                else
+                    stack.pop();
+            }
+
+        while (!stack.empty())
+            s[stack.top()] = '*', stack.pop();
+
+        s.erase(remove(s.begin(), s.end(), '*'), s.end());
+        return s;
+    }
+};
