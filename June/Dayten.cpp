07ad9d65c2d41b76776393d85a9cdbc53c9b62d5
Pranjali Bhardwaj <146981751+PranjaliBhardwaj@users.noreply.github.com>
2024-06-10 21:01:03 +0530

    Create Dayten.cpp

---
+++ June/Dayten.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<char> sorted(heights.begin(), heights.end());
+        sort(heights.begin(), heights.end());
+        int n=heights.size(), ans=0;
+        for(int i=0; i<n; i++)
+            ans+=(heights[i]!=sorted[i]);
+        return ans;      
+    }
+};
+
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
